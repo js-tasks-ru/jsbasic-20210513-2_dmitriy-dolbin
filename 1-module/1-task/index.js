c18873249239fbd79b6d2e5890a4d50ff42ecd76
Doctor_Lector <roller97@yandex.ru>
2021-05-30 20:43:39 +0300

    done first task from module first

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1)
+    return 1;
+  else {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+      result = result * (n - i);
+      console.log("result", result);
+    }
+    return result;
+  }
 }
+factorial(3);
