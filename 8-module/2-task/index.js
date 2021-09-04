604cc51f71bd5d33b9fc29208a73c34b252fb630
Doctor_Lector <roller97@yandex.ru>
2021-09-04 15:58:22 +0300

    done all task from 8 module

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,156 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.productFilter = [];
+    
+    this.elem = document.createElement('div');
+    this.render();
+    
   }
+
+  render() {
+
+    this.elem.classList.toggle('products-grid');
+    
+    const childElem = document.createElement('div');
+    childElem.classList.toggle('products-grid__inner');
+    this.renderCard(childElem);
+
+    this.elem.appendChild(childElem);
+
+  }
+
+  renderCard(childElem) {
+
+    let productCards = new Array();
+    
+    if (this.productFilter.length > 0 || Object.keys(this.filters).length > 0) {
+      productCards = this.productFilter;
+    } else {
+      productCards = this.products;
+    }
+
+    const nCntProduct = productCards.length;
+    for (let iProduct = 0; iProduct < nCntProduct; iProduct++) {
+      
+      const elemCard = new ProductCard(productCards[iProduct]);
+      childElem.appendChild(elemCard.elem);
+      
+    }
+
+  }
+
+  showProductNoNuts(products) {
+
+    const nCntProduct = products.length;
+    const newProductNoNuts = new Array();
+
+    for (let iProduct = 0; iProduct < nCntProduct; iProduct++) {
+      
+      if (products[iProduct]["nuts"] === false || !("nuts" in products[iProduct])) {
+        newProductNoNuts.push(products[iProduct]);
+      }
+
+    }
+    return newProductNoNuts;
+
+  }
+
+  showProductVegeterian(products) {
+
+    const nCntProduct = products.length;
+    const newProductVegetarian = new Array();
+
+    for (let iProduct = 0; iProduct < nCntProduct; iProduct++) {
+      
+      if (products[iProduct]["vegeterian"] === true) {
+        newProductVegetarian.push(products[iProduct]);
+      }
+
+    }
+    return newProductVegetarian;
+
+  }
+
+  showProductMaxSpiciness(products, maxSpiciness) {
+    
+    const nCntProduct = products.length;
+    const newProductMaxSpiciness = new Array();
+    
+    for (let iProduct = 0; iProduct < nCntProduct; iProduct++) {
+      
+      if (Number(products[iProduct]["spiciness"]) <= Number(maxSpiciness)) {
+        newProductMaxSpiciness.push(products[iProduct]);
+      }
+
+    }
+    return newProductMaxSpiciness;
+
+  }
+  
+  showProductCategory(products, category) {
+    
+    const nCntProduct = products.length;
+    const newProductCategory = new Array();
+
+    for (let iProduct = 0; iProduct < nCntProduct; iProduct++) {
+      
+      if (products[iProduct]["category"] === category) {
+        newProductCategory.push(products[iProduct]);
+      }
+
+    }
+    return newProductCategory;
+
+  }
+
+  applyFilter() {
+
+    this.productFilter = this.products;
+    if (this.filters["noNuts"] === true) {
+      this.productFilter = this.showProductNoNuts(this.productFilter);
+    }
+    if (this.filters["vegeterianOnly"] === true) {
+      this.productFilter = this.showProductVegeterian(this.productFilter);
+    }
+
+    if (this.filters["maxSpiciness"]) {
+      this.productFilter = this.showProductMaxSpiciness(this.productFilter, this.filters["maxSpiciness"]);
+    }
+
+    if (this.filters["category"]) {
+      this.productFilter = this.showProductCategory(this.productFilter, this.filters["category"]);
+    }
+
+  }
+
+  removeCards() {
+
+    let cards = this.elem.querySelectorAll('.card');
+    const nCntCards = cards.length;
+    for (let iCard = 0; iCard < nCntCards; iCard++) {
+      cards[iCard].remove();
+    }
+
+  }
+
+  updateFilter(filters) {
+
+    if (Object.keys(filters).length === 1) {
+      
+      const nameFilter = Object.keys(filters)[0];
+      if (!(nameFilter in this.filters) || this.filters[nameFilter] !== filters[nameFilter]) {
+        this.filters[nameFilter] = filters[nameFilter];
+      }
+
+    }
+    this.removeCards();
+    this.applyFilter();
+
+    const childElem = document.querySelector('.products-grid__inner');
+    this.renderCard(childElem);
+    this.productFilter = [];
+    
+  }
+
 }
--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -14,22 +14,90 @@ export default class Cart {
 
   addProduct(product) {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    function createCard(productItem) {
+
+      const card = {};
+      card["product"] = productItem;
+      card["count"] = 1;
+      return card;
+
+    }
+
+    if (product) {
+
+      const aProducts = new Array();
+      const nCntCard = this.cartItems.length;
+      for (let iCard = 0; iCard < nCntCard; iCard++) {
+        aProducts.push(this.cartItems[iCard]["product"]["id"]);
+      }
+
+      const nIndex = aProducts.indexOf(product["id"]);
+      if (nIndex > -1) {
+        
+        this.cartItems[nIndex]["count"]++;
+        this.onProductUpdate(this.cartItems[nIndex]);
+
+      } else {
+        
+        const productCard = createCard(product);
+        this.cartItems.push(productCard);
+        this.onProductUpdate(this.cartItems[this.cartItems.length - 1]);
+
+      }
+
+    }
+
   }
 
   updateProductCount(productId, amount) {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    if (!this.isEmpty()) {
+
+      for (let iCard = 0; iCard < this.cartItems.length; iCard++) {
+        
+        if (this.cartItems[iCard]["product"]["id"] === productId) {
+          this.cartItems[iCard]["count"] += amount;
+          this.onProductUpdate(this.cartItems[iCard]);
+        }
+
+        if (this.cartItems[iCard]["count"] === 0) {
+          this.cartItems.splice(iCard, 1);
+        }
+
+      }
+
+    }
+
   }
 
   isEmpty() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    if (this.cartItems.length > 0) {
+      return false;
+    }
+    return true;
   }
 
   getTotalCount() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    const nCntCard = this.cartItems.length;
+    let nTotalCnt = 0;
+    for (let iCard = 0; iCard < nCntCard; iCard++) {
+      nTotalCnt = nTotalCnt + this.cartItems[iCard]["count"];
+    }
+    return nTotalCnt;
+
   }
 
   getTotalPrice() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    const nCntCard = this.cartItems.length;
+    let nTotalPrice = 0;
+    for (let iCard = 0; iCard < nCntCard; iCard++) {
+      nTotalPrice = nTotalPrice + (this.cartItems[iCard]["count"] * this.cartItems[iCard]["product"]["price"]);
+    }
+    return nTotalPrice;
+
   }
 
   renderProduct(product, count) {
@@ -83,18 +151,128 @@ export default class Cart {
     </form>`);
   }
 
+  onClickBtn(event, amount) {
+    const productId = event.target.closest('div').parentNode.parentNode.parentNode.getAttribute("data-product-id");
+    this.updateProductCount(productId, amount);
+  }
+
   renderModal() {
     // ...ваш код
+    const modal = new Modal();
+    modal.setTitle("Your order");
+    
+    const nCntCard = this.cartItems.length;
+    for (let iCard = 0; iCard < nCntCard; iCard++) {
+      modal.setBody(this.renderProduct(this.cartItems[iCard]["product"], this.cartItems[iCard]["count"]));
+    }
+    
+    if (!document.body.classList.contains('is-modal-open')) {
+      
+      modal.open();
+
+      const elemBtn = document.querySelectorAll('.cart-counter__button');
+      const nCntBtn = elemBtn.length;
+      for (let iBtn = 0; iBtn < nCntBtn; iBtn++) {
+      
+        if (elemBtn[iBtn].classList.contains('cart-counter__button_minus')) {
+          elemBtn[iBtn].onclick = () => this.onClickBtn(event, -1);
+        } else {
+          elemBtn[iBtn].onclick = () => this.onClickBtn(event, 1);
+        }
+      
+      }
+
+    }
+
+    const modalTitle = document.querySelector('.modal__title');
+    modalTitle.textContent = modalTitle.textContent.trim();
+
+    const modalBody = document.querySelector('.modal__body');
+    const formDataDelivery = this.renderOrderForm();
+    formDataDelivery["id"] = "form";
+    formDataDelivery.onsubmit = () => this.onSubmit(event);
+    modalBody.appendChild(formDataDelivery);
+
   }
 
   onProductUpdate(cartItem) {
     // ...ваш код
-
     this.cartIcon.update(this);
+    if (document.body.classList.contains('is-modal-open')) {
+      
+      const productId = cartItem["product"]["id"];
+      const productCount = document.querySelector(`[data-product-id="${productId}"] .cart-counter__count`);
+      const productPrice = document.querySelector(`[data-product-id="${productId}"] .cart-product__price`);
+      const infoPrice = document.querySelector(`.cart-buttons__info-price`);
+
+      if (cartItem["count"] > 0) {
+      
+        productCount.innerHTML = cartItem["count"];
+        productPrice.innerHTML = `€${(cartItem["count"] * cartItem["product"]["price"]).toFixed(2)}`;
+        infoPrice.innerHTML = `€${this.getTotalPrice().toFixed(2)}`;
+
+      } else {
+
+        const productDiv = document.querySelector(`[data-product-id="${productId}"]`);
+        productDiv.parentNode.removeChild(productDiv);
+        if (this.cartItems.length === 1) {
+          
+          const modalDiv = document.querySelector('.modal');
+          modalDiv.parentNode.removeChild(modalDiv);
+          document.body.classList.toggle('is-modal-open');
+
+        }
+
+      }
+
+    }
+
+  }
+
+  async fetchPost(postData) {
+    
+    const response = await fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: postData
+    });
+
+    if (response.ok) {
+      return true;
+    }
+    return false;
+
   }
 
   onSubmit(event) {
     // ...ваш код
+    event.preventDefault();
+    const btnOrder = document.querySelector('.btn-group__button');
+    btnOrder.classList.toggle('is-loading');
+
+    const modalTitle = document.querySelector('.modal__title');
+    modalTitle.textContent = 'Success!';
+
+    const formData = document.getElementById('form');
+    const formDataPost = new FormData();
+    formDataPost.set("name", formData.querySelector('[name="name"]')["value"]);
+    formDataPost.set("email", formData.querySelector('[name="email"]')["value"]);
+    formDataPost.set("tel", formData.querySelector('[name="tel"]')["value"]);
+    formDataPost.set("address", formData.querySelector('[name="address"]')["value"]);
+
+    if (this.fetchPost(formDataPost)) {
+      this.cartItems = [];
+      const modalBody = document.querySelector('.modal__body');
+      modalBody.innerHTML = `
+      <div class="modal__body-inner">
+        <p>
+          Order successful! Your order is being cooked :) <br>
+          We’ll notify you about delivery time shortly.<br>
+          <img src="/assets/images/delivery.gif">
+        </p>
+      </div>
+      `;
+    }
+
   };
 
   addEventListeners() {
